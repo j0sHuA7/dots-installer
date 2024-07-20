224794709f6be0e3a8a1b3ed3f3402e16cbbb22c
j0sHuA7 <j02huad4n1e1@gmail.com>
2024-07-20 18:17:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,17 +3,18 @@ echo -e "\e[34mWelcome to my dots installer!\e[m"
        	git clone https://gitea.com/yehoshua/config/ 
         mkdir ~/.config
 	mkdir ~/.config/i3
-	mkdir ~/.config/kitty
+	mkdir ~/.config/kitty/
 	mkdir ~/.local/
 	mkdir ~/.local/share
 	mkdir ~/.local/share/fonts/
+        mkdir ~/.config/picom/
 	wget https://files.ax86.net/terminus-ttf/files/latest.zip
 	unzip latest.zip 
 	rm latest.zip
 	mv terminus-ttf-* ~/.local/share/fonts
 } &> /dev/null
-
+mv config/picom.conf ~/.config/picom/
 mv config/config ~/.config/i3/
-mv config/kitty ~/.config/kitty/
+mv config/kitty.conf ~/.config/kitty/
 mv config/wall.jpg ~
 
