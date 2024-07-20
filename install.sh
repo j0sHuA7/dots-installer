b4b81e070ed568b87ce26661775d7f626f596983
Joshua <j02huad4n1e1@gmail.com>
2024-07-20 18:36:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo -e "\e[34mWelcome to my dots installer!\e[m"
 echo -e "\e[31mThis may take some time\e[m..."
 {
-       	git clone https://gitea.com/yehoshua/config/ 
+	git clone https://gitea.com/yehoshua/config/ 
         mkdir ~/.config
 	mkdir ~/.config/i3
 	mkdir ~/.config/kitty/
@@ -14,6 +14,7 @@ echo -e "\e[31mThis may take some time\e[m..."
 	rm latest.zip
 	mv terminus-ttf-* ~/.local/share/fonts
 } &> /dev/null
+mv config/.bashrc ~
 mv config/picom.conf ~/.config/picom/
 mv config/config ~/.config/i3/
 mv config/kitty.conf ~/.config/kitty/
